22e963bfa052f220c912182ada17a9c46d98a89c
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-18 13:01:18 -0700

    Created tag delete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,24 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete one product by its `id` value
+  try {
+    const tag = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
