8434a5b429d41e8f9cf1540e765f0a97f05d19ff
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-18 18:36:11 -0700

    Finished Tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tag = await Tag.findAll({});
+    const tag = await Tag.findAll({include: [{ model: Product }]});
     res.json(tag);
   } catch(err) {
     console.log("Hello Error");
@@ -15,11 +15,25 @@ router.get('/', async (req, res) => {
   }
   });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  res.send("Hello Georgie!!")
   // be sure to include its associated Product data
-});
+  try {
+    const tag = await Tag.findByPk(req.params.id, {include: [{ model: Product }]});
+    
+      
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id! You should create one :)' });
+      return;
+    }
+
+    res.json(tag);
+
+  } catch(err) {
+    console.log("Hello Error");
+  if(err) throw new Error(err);
+  }
+  });
 
 router.post('/', async (req, res) => {
 
@@ -32,24 +46,27 @@ router.post('/', async (req, res) => {
   .catch((err) => {
     res.json(err);
   });
-  // try {
-  //   const userData = await User.create(req.body);
-  //   // 200 status code means the request is successful
-  //   res.status(200).json(userData);
-  // } catch (err) {
-  //   // 400 status code means the server could not understand the request
-  //   res.status(400).json(err);
-  // }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tag = await Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }) 
+  res.json(tag)
+} catch(err) {
+  console.log("Hello Error");
+if(err) throw new Error(err);
+}
 });
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const tag = await Product.destroy({
+    const tag = await Tag.destroy({
       where: {
         id: req.params.id
       }
